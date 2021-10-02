fdc39ebe2704b85cb5f0b70fc2be1690834b1fa7
u18c70 <juergen.minwegen@avl.com>
2021-10-02 20:02:39 +0200

    first big commit

---
+++ Assets/Scripts/PopUpSystem.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class PopUpSystem : MonoBehaviour
+{
+    public GameObject popUpBox;
+    public Animator animator;
+    public TMP_Text popupName;
+    public TMP_Text pupupDescription;
+
+    public void PopUp(string name, string description)
+    {
+        popUpBox.SetActive(true);
+        popupName.text = name;
+        pupupDescription.text = description;
+        animator.SetTrigger("open");
+    }
+}
