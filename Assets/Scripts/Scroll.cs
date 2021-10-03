2f1bfc51eb34bb09a9e2882b6b04e8f78dccd3c2
Patrick Seebacher <patrick.seebacher@gmail.com>
2021-10-03 15:48:21 +0200

    Scroll Script Update

--- Assets/Scripts/Scroll.cs
+++ Assets/Scripts/Scroll.cs
@@ -1,18 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class Scroll : MonoBehaviour
 {
-    private ScrollRect _scrollRect;
+    private List<string> _history;
+    private string _questText;
 
+    private ScrollRect _scrollRect;
     private int _scrollActive = 0;
+
+    private TMPro.TextMeshProUGUI _uiText;
 
     // Start is called before the first frame update
     void Start()
     {
         _scrollRect = this.GetComponentInChildren<ScrollRect>();
+        _uiText = this.GetComponentInChildren<TMPro.TextMeshProUGUI>();
+        _history = new List<string>();
     }
 
     // Update is called once per frame
@@ -30,5 +37,31 @@ public class Scroll : MonoBehaviour
     public void ButtonOnUp()
     {
         _scrollActive = 0;
+    }
+
+    public void AddHistoryItem(string item)
+    {
+        _history.Add(item);
+
+        if (_history.Count >= 10) _history.RemoveAt(0);
+
+        UpdateScrollText();
+    }
+
+    public void SetNewQuestText(string quest)
+    {
+        _questText = quest;
+
+        UpdateScrollText();
+    }
+
+    public void UpdateScrollText()
+    {
+        _uiText.text = _questText + "\n\n";
+
+        for (int i = 0; i < _history.Count; i++)
+        {
+            _uiText.text += "* " + _history[_history.Count - 1 - i];
+        }
     }
 }
