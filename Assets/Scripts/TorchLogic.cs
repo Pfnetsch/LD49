0b095c2c39e6daa9711cde3c534dad9ac836f32a
Jürgen Minwegen <juergen.minwegen@apoke.org>
2021-10-03 23:57:23 +0200

    reimplemented torch. deleted old Fackel. It works now

--- Assets/Scripts/TorchLogic.cs
+++ Assets/Scripts/TorchLogic.cs
@@ -4,15 +4,51 @@ using UnityEngine;
 
 public class TorchLogic : MonoBehaviour
 {
+    GameObject ld;
+    SpriteRenderer sprite;
+    public Animator animator;
+    bool isLit = true;
+    Cauldron cauldron;
     // Start is called before the first frame update
     void Start()
     {
-        
+        ld = GameObject.Find("LightRect");
+        sprite = ld.GetComponent<SpriteRenderer>();
+        sprite.color = new Color(.1f, .1f, .3f, 0f);
+        cauldron = FindObjectOfType<Cauldron>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector2 origin = new Vector2(mousePos.x, mousePos.y);
+            RaycastHit2D collisionHit = Physics2D.Raycast(origin, Vector2.zero, 0f);
+
+            if (collisionHit)
+            {
+                if (collisionHit.transform.CompareTag("Torch"))
+                {
+                    if(isLit == true)
+                    {
+                        animator.SetTrigger("smoke");
+                        sprite.color = new Color(.1f, .1f, .3f, .5f);
+                        isLit = false;
+                        cauldron.Lumi = PotionDB.Luminosity.Dark;
+                    }
+                    else
+                    {
+                        animator.SetTrigger("burn");
+                        sprite.color = new Color(.1f, .1f, .3f, 0f);
+                        isLit = true;
+                        cauldron.Lumi = PotionDB.Luminosity.Bright;
+                    }
+                }
+            }
+        }
     }
 }
