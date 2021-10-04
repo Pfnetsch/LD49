0b78775a7f729fb3a93c9bde7196003d96585bed
Patrick Seebacher <patrick.seebacher@gmail.com>
2021-10-04 12:11:06 +0200

    Update narratorScript.cs

--- Assets/Scripts/narratorScript.cs
+++ Assets/Scripts/narratorScript.cs
@@ -9,33 +9,58 @@ public class narratorScript : MonoBehaviour
     public GameObject popUpBox;
     public TMP_Text pupupDescription;
 
-   
+    private Coroutine _coroutine;
+    private int _idxText;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        StartCoroutine(waiter());
+        _idxText = 0;
+        _coroutine = StartCoroutine(waiter());
+    }
+
+    void Update()
+    {
+        if (Input.anyKeyDown)
+        {
+            StopCoroutine(_coroutine);
+            _idxText++;
+            _coroutine = StartCoroutine(waiter());
+        }
     }
 
     IEnumerator waiter()
     {
-        pupupDescription.text = "ME: Ah … u-uh … my head … hurts. And I feel sick … uh, where am I? And what happened?";
+        switch (_idxText)
+        {
+            case 0:
+                pupupDescription.text = "ME: Ah … u-uh … my head … hurts. And I feel sick … uh, where am I? And what happened?";
+                yield return new WaitForSecondsRealtime(5);
+                break;
 
-        yield return new WaitForSecondsRealtime(5);
+            case 1:
+                pupupDescription.text = "ME:… What’s that on my hand?";
+                yield return new WaitForSecondsRealtime(5);
+                break;
 
-        pupupDescription.text = "ME:… What’s that on my hand?";
+            case 2:
+                pupupDescription.fontStyle = FontStyles.Italic;
+                pupupDescription.text = "Hey Buddy, the witch party was pretty crazy yesterday, huh? You seemed kinda wasted. We brought you to the alchemy room so you won’t be late for your exam. Good luck with it!!!";
+                yield return new WaitForSecondsRealtime(8);
+                break;
+            case 3:
+                pupupDescription.fontStyle = FontStyles.Normal;
+                pupupDescription.text = "ME:What … party? What exam?!";
+                yield return new WaitForSecondsRealtime(5);
+                break;
 
-        yield return new WaitForSecondsRealtime(5);
-        pupupDescription.fontStyle = FontStyles.Italic;
-        pupupDescription.text = "Hey Buddy, the witch party was pretty crazy yesterday, huh? You seemed kinda wasted. We brought you to the alchemy room so you won’t be late for your exam. Good luck with it!!!";
+            default:
+                SceneManager.LoadScene(2);
+                break;
+        }
 
-        yield return new WaitForSecondsRealtime(8);
-
-        pupupDescription.fontStyle = FontStyles.Normal;
-        pupupDescription.text = "ME:What … party? What exam?!";
-
-        yield return new WaitForSecondsRealtime(5);
-
-        SceneManager.LoadScene(2);
+        _idxText++;
+        _coroutine = StartCoroutine(waiter());
     }
 }
