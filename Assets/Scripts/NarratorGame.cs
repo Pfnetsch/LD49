234de2187ec63e7d05456bf78ba2965e7dbdfe06
Patrick Seebacher <patrick.seebacher@gmail.com>
2021-10-04 19:25:54 +0200

    Narrator fixed

--- Assets/Scripts/NarratorGame.cs
+++ Assets/Scripts/NarratorGame.cs
@@ -17,7 +17,7 @@ public class NarratorGame : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        NarratorBox.SetActive(false);
+        //NarratorBox.SetActive(false);
     }
 
     void Update()
